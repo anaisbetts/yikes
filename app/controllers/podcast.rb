319e1ab30d6747bcc8e7612405ddd02e921ca9ea
Paul Betts <paul@paulbetts.org>
2008-05-30 17:36:08 -0700

    Write podcast generation code - still fails in iTunes. Code moved to controller because Merb can't deal with local variables in XML views

--- app/controllers/podcast.rb
+++ app/controllers/podcast.rb
@@ -1,5 +1,47 @@
-class Podcast < Application
-	provides :xml
+require 'builder'
+require 'builder/xmlmarkup'
 
-	def index; render; end
+class Podcast < Application
+	def index; 
+		render podcast_xml, :layout => false
+	end
+
+private
+	def podcast_xml
+		app = Yikes.instance.state
+		
+		xml = Builder::XmlMarkup.new
+		xml.instruct!
+
+		xml.rss("xmlns:itunes" => "http://www.itunes.com/dtds/podcast-1.0.dtd", "xmlns:media" => "http://search.yahoo.com/mrss/", "version" => "2.0") do
+
+		xml.channel do
+			xml.title "Yikes video feed of #{Pathname.new(app.library).basename.to_s}"
+			xml.link url_base
+			xml.language "en-us"
+			xml.copyright "Copyright 2008 Paul Betts"
+			xml.tag! "itunes:summary", "Converted videos, from #{app.library}"
+			xml.description "Converted videos, from #{app.library}"
+			xml.tag! "itunes:image", "#{url_base}/FILLMEIN.png"
+			xml.tag! "itunes:category", "Videos"
+
+			app.get_finished_items.each do |x|
+				xml.item do
+					next unless x.succeeded
+					xml.title x.subpath
+					xml.tag! "itunes:subtitle", x.source_path
+					xml.enclosure(:url => "#{url_base}/files/#{x.subpath}", :length => "100000", :type => "video/mp4")
+					xml.guid "#{url_base}/#{Platform.hostname}.local/files/#{x.subpath}"
+					xml.pubDate x.finished_at.to_s
+					xml.tag! "itunes:duration", "3:00"
+				end 
+			end
+		end ; end
+
+		xml.target!
+	end
+
+	def url_base
+		"http://#{Platform.hostname}.local:4000"
+	end
 end
